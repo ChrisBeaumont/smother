07ac1490aa29c34c6abfd72ef3a43c632acee022
Chris Beaumont <cbeaumont@counsyl.com>
2016-06-10 16:55:04 -0700

    Document requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ setup(
     name='smother',
     version='0.0.1',
     packages=find_packages(),
+    install_requires = [
+        'click',
+        'diff_cover>=0.9',
+        'more_itertools',
+        'coverage',
+    ],
     entry_points={
         'nose.plugins.0.10': [
             'smother = smother.nose_plugin:SmotherNose',
