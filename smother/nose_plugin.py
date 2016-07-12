8dfd53134d005de461a11c51d8ec28fd44cf6741
Chris Beaumont <cbeaumont@counsyl.com>
2016-07-11 20:12:11 -0700

    Strip down nosetests args

--- smother/nose_plugin.py
+++ smother/nose_plugin.py
@@ -40,57 +40,11 @@ class SmotherNose(Coverage):
                           dest="cover_erase",
                           help="Erase previously collected coverage "
                           "statistics before run")
-        parser.add_option("--smother-tests", action="store_true",
-                          dest="cover_tests",
-                          default=env.get('NOSE_COVER_TESTS'),
-                          help="Include test modules in coverage report "
-                          "[NOSE_COVER_TESTS]")
-        parser.add_option("--smother-min-percentage", action="store",
-                          dest="cover_min_percentage",
-                          default=env.get('NOSE_COVER_MIN_PERCENTAGE'),
-                          help="Minimum percentage of coverage for tests "
-                          "to pass [NOSE_COVER_MIN_PERCENTAGE]")
-        parser.add_option("--smother-inclusive", action="store_true",
-                          dest="cover_inclusive",
-                          default=env.get('NOSE_COVER_INCLUSIVE'),
-                          help="Include all python files under working "
-                          "directory in coverage report.  Useful for "
-                          "discovering holes in test coverage if not all "
-                          "files are imported by the test suite. "
-                          "[NOSE_COVER_INCLUSIVE]")
-        parser.add_option("--smother-html", action="store_true",
-                          default=env.get('NOSE_COVER_HTML'),
-                          dest='cover_html',
-                          help="Produce HTML coverage information")
-        parser.add_option('--smother-html-dir', action='store',
-                          default=env.get('NOSE_COVER_HTML_DIR', 'cover'),
-                          dest='cover_html_dir',
-                          metavar='DIR',
-                          help='Produce HTML coverage information in dir')
-        parser.add_option("--smother-branches", action="store_true",
-                          default=env.get('NOSE_COVER_BRANCHES'),
-                          dest="cover_branches",
-                          help="Include branch coverage in coverage report "
-                          "[NOSE_COVER_BRANCHES]")
-        parser.add_option("--smother-xml", action="store_true",
-                          default=env.get('NOSE_COVER_XML'),
-                          dest="cover_xml",
-                          help="Produce XML coverage information")
-        parser.add_option("--smother-xml-file", action="store",
-                          default=env.get('NOSE_COVER_XML_FILE',
-                                          'coverage.xml'),
-                          dest="cover_xml_file",
-                          metavar="FILE",
-                          help="Produce XML coverage information in file")
         parser.add_option("--smother-config-file", action="store",
                           default=env.get('NOSE_COVER_CONFIG_FILE'),
                           dest="cover_config_file",
                           help="Location of coverage config file "
                           "[NOSE_COVER_CONFIG_FILE]")
-        parser.add_option("--smother-no-print", action="store_true",
-                          default=env.get('NOSE_COVER_NO_PRINT'),
-                          dest="cover_no_print",
-                          help="Suppress printing of coverage information")
         parser.add_option("--smother-output", action="store",
                           default=env.get('NOSE_SMOTHER_OUTPUT', '.smother'),
                           dest="smother_output",
